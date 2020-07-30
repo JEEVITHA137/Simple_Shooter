fdfda4b74601e48b0c560057fd1d9e4b2e55b614
jeevitha <jeevithasakthi137@gmail.com>
2020-07-30 20:42:46 +0530

    Custom BTTasks in c++

---
+++ Source/SimpleShooter/BTTask_ClearBlackboardBase.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_ClearBlackboardBase.h"
+
+
+UBTTask_ClearBlackboardBase::UBTTask_ClearBlackboardBase() 
+{
+    NodeName = TEXT("Clear Board Value");
+}
