4683139fb9dd9680a478ff420b4dda7bfdda33d2
JEEVITHA137 <47686236+JEEVITHA137@users.noreply.github.com>
2020-08-08 05:33:11 -0700

    HealthBars

--- Source/SimpleShooter/ShooterCharacter.cpp
+++ Source/SimpleShooter/ShooterCharacter.cpp
@@ -53,6 +53,11 @@ bool AShooterCharacter::IsDead() const
 	return Health<=0;
 }
 
+float AShooterCharacter::GetHealthPercent() const
+{
+	return Health / MaxHealth;
+}
+
 // Called every frame
 void AShooterCharacter::Tick(float DeltaTime)
 {
